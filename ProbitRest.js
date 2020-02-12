ac96f8ff968b413ef86fca13999329eea1e91402
Jason Sowers <jason.w.sowers@gmail.com>
2020-02-11 19:37:34 -0800

    added token method

--- ProbitRest.js
+++ ProbitRest.js
@@ -1,2 +1,25 @@
+const fetch = require('node-fetch');
+
 class ProbitRest {
+    constructor(key, secret) {
+        this.key = key;
+        this.secret = secret;
+        this.tokenUrl = 'https://accounts.probit.com/"';
+    }
+
+    async token(key, secret) {
+        const res = await fetch(`${this.tokenUrl}/token`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + Base64.encode(`${key}:${secret}`)
+            },
+            body: JSON.stringify({ grant_type: 'client_credentials' })
+        });
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
 }
+module.exports = ProbitRest;
