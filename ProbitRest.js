3675c4f55ea366639b5d46f95359505f6941aaca
Jason Sowers <jason.w.sowers@gmail.com>
2020-02-11 23:38:32 -0800

    fixes and added endpoints

--- ProbitRest.js
+++ ProbitRest.js
@@ -4,23 +4,28 @@ class ProbitRest {
     constructor(key, secret) {
         this.key = key;
         this.secret = secret;
-        this.tokenUrl = 'https://accounts.probit.com/"';
-        this.exchangeUrl = 'https://api.probit.com/api/exchange/v1/';
+        this.tokenUrl = 'https://accounts.probit.com';
+        this.exchangeUrl = 'https://api.probit.com/api/exchange/v1';
     }
 
-    async token(key, secret) {
+    async token() {
+        console.log(this.key);
         const res = await fetch(`${this.tokenUrl}/token`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + Base64.encode(`${key}:${secret}`)
+                'Authorization': 'Basic ' + new Buffer(`${this.key}:${this.secret}`).toString('base64')
             },
             body: JSON.stringify({ grant_type: 'client_credentials' })
         });
         if (!res.ok) {
             throw new Error(res.statusText);
         }
-        return res.json();
+        try {
+            return res.json();
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     async market() {
@@ -71,6 +76,58 @@ class ProbitRest {
         return res.json();
     }
 
+    async newLimitOrder(markerId, side, timeInForce, limitPrice, quantity) {
+        const tokenResponse = await this.token();
+        const res = await fetch(`${this.exchangeUrl}/new_order`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': 'Bearer ' + tokenResponse.access_token
+            },
+            body: JSON.stringify({
+                "market_id": markerId,
+                "type": "limit",
+                "side": side,
+                "time_in_force": timeInForce,
+                "limit_price": limitPrice,
+                "quantity": quantity
+            })
+        });
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        try {
+            return res.json();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async newMarketOrder(marketId, quantity, side, timeInForce) {
+        const tokenResponse = await this.token();
+        const res = await fetch(`${this.exchangeUrl}/new_order`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': 'Bearer ' + tokenResponse.access_token
+            },
+            body: JSON.stringify({
+                "market_id": marketId,
+                "quantity": quantity,
+                "side": side,
+                "time_in_force": timeInForce,
+                "type": "market"
+            })
+        });
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        try {
+            return res.json();
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
 
 }
