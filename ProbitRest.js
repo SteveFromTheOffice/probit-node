05579a1365b94c62497b5ddcdbb81ad51f376792
Jason Sowers <jason.w.sowers@gmail.com>
2020-02-11 22:04:02 -0800

    added public endpoints

--- ProbitRest.js
+++ ProbitRest.js
@@ -5,6 +5,7 @@ class ProbitRest {
         this.key = key;
         this.secret = secret;
         this.tokenUrl = 'https://accounts.probit.com/"';
+        this.exchangeUrl = 'https://api.probit.com/api/exchange/v1/';
     }
 
     async token(key, secret) {
@@ -21,5 +22,56 @@ class ProbitRest {
         }
         return res.json();
     }
+
+    async market() {
+        const res = await fetch(`${this.exchangeUrl}/market`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+    async currency() {
+        const res = await fetch(`${this.exchangeUrl}/currency`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+    async time() {
+        const res = await fetch(`${this.exchangeUrl}/time`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+    async ticker(tickers) {
+        const res = await fetch(`${this.exchangeUrl}/time?market_ids=${tickers.join(',')}`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+    async orderBook(marketId) {
+        const res = await fetch(`${this.exchangeUrl}/ticker?market_id=${marketId}`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+    async trade(marketId, startTime, endTime, limit) {
+        const res = await fetch(`${this.exchangeUrl}/trade?market_id=${marketId}&start_time=${startTime.toISOString()}&end_time=${endTime}&limit=${limit}`);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+        return res.json();
+    }
+
+
+
 }
 module.exports = ProbitRest;
